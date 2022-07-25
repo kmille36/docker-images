d1d7c418a3596cc20343a6f4a606fe81a0e09595
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-07-26 02:35:42 +0700

    Update entrypoint.sh

--- parrot/entrypoint.sh
+++ parrot/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo -e "\nhttps://github.com/qeeqbox/docker-images\n\nCustom Parrot OS distro accessible via VNC, RDP or web"
 x1=$(hostname -I | cut -d' ' -f1)
-x2=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#%^&*_+-=' | fold -w 10 | head -n 1)
-x3=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#%^&*_+-=' | fold -w 10 | head -n 1)
-x4=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#%^&*_+-=' | fold -w 10 | head -n 1)
+x2=password
+x3=password
+x4=password
 echo -e "\nroot pass -> $x2\n-----------------------\nUsername  -> xuser\nPassword  -> $x3\nVNC pass  -> $x4\n\nhttp://$x1:6080/index.html\n"
 echo root:$x2 | chpasswd
 echo xuser:$x3 | chpasswd
